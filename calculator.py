5d828e337fed2b552cda0e2640996532063fe4db
harshit <harshit.jagarlamudi@outlook.com>
2024-06-22 18:25:48 +0100

    change ui

--- calculator.py
+++ calculator.py
@@ -35,37 +35,37 @@ def delete():
     text_results.delete("end-2c", "end-1c")
 
 
-root = tk.Tk()
-root.geometry("320x420")
-text_results = tk.Text(root, height=1, width=16, font=("Arial",24))
-text_results.grid(columnspan=5)
+root = tk.Tk() 
+root.geometry("290x375")
+text_results = tk.Text(root, height=1, width=16, font=("Consolas",24))
+text_results.grid(columnspan=5,sticky='nesw')
 #number keys
-btn1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial",14))
-btn1.grid(row=4, column=1)
-btn2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial",14))
-btn2.grid(row=4, column=2)
-btn3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial",14))
-btn3.grid(row=4, column=3)
-btn4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font=("Arial",14))
-btn4.grid(row=3, column=1)
-btn5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font=("Arial",14))
-btn5.grid(row=3, column=2)
-btn6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, font=("Arial",14))
-btn6.grid(row=3, column=3)
-btn7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, font=("Arial",14))
-btn7.grid(row=2, column=1)
-btn8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font=("Arial",14))
-btn8.grid(row=2, column=2)
-btn9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial",14))
-btn9.grid(row=2, column=3)
-btn0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=12, font=("Arial",14))
-btn0.grid(row=6,column=1,columnspan=2   )
+btn1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn1.grid(row=4, column=1,sticky='nesw')
+btn2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn2.grid(row=4, column=2,sticky='nesw')
+btn3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn3.grid(row=4, column=3,sticky='nesw')
+btn4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn4.grid(row=3, column=1,sticky='nesw')
+btn5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn5.grid(row=3, column=2,sticky='nesw')
+btn6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn6.grid(row=3, column=3,sticky='nesw')
+btn7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn7.grid(row=2, column=1,sticky='nesw')
+btn8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn8.grid(row=2, column=2,sticky='nesw')
+btn9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn9.grid(row=2, column=3,sticky='nesw')
+btn0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=12, font=("Consolas",14), bg="white",highlightthickness = 5, bd = 0)
+btn0.grid(row=6,column=1,columnspan=2,sticky='nesw')
 
 #functions
-btndel = tk.Button(root, text="del", command=delete ,width=5, font=("Arial",14))
-btndel.grid(row=6, column=3)
+btndel = tk.Button(root, text="del", command=delete ,width=5, font=("Consolas",14), bg="white",highlightthickness =0, bd = 0)
+btndel.grid(row=6, column=3,sticky='nesw')
 
-btnadd = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), font=("Arial", 14))
+btnadd = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), font=("Consolas", 14), highlightthickness = 0, bd = 0)
 
 
 # Load the image and set it to the button
@@ -75,32 +75,35 @@ add = ImageTk.PhotoImage(file="Untitled.png")
 btnadd.config(image=add)
 
 # Use grid layout manager to place the button
-btnadd.grid(row=2, column=4)
+btnadd.grid(row=2, column=4,sticky='nesw')
 
 
-btnminus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"),  font=("Arial",14))
-btnminus.grid(row=3, column=4)
+btnminus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"),  font=("Consolas",14),highlightthickness = 0, bd = 0)
+btnminus.grid(row=3, column=4,sticky='nesw')
 minus = ImageTk.PhotoImage(file="minus.png")
 btnminus.config(image=minus)
 
 
 
-btnmultiply = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), font=("Arial",14))
-btnmultiply.grid(row=4, column=4)
+btnmultiply = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), font=("Consolas",14),highlightthickness = 0, bd = 0)
+btnmultiply.grid(row=4, column=4,sticky='nesw')
 multi = ImageTk.PhotoImage(file="multiply.png")
 btnmultiply.config(image=multi)
 
 
-btndivide = tk.Button(root, text="/", command=lambda: add_to_calculation("/"),  font=("Arial",14))
-btndivide.grid(row=6, column=4)
+btndivide = tk.Button(root, text="/", command=lambda: add_to_calculation("/"),  font=("Consolas",14),highlightthickness = 0, bd = 0)
+btndivide.grid(row=6, column=4,sticky='nesw')
 divide = ImageTk.PhotoImage(file="divide.png")
 btndivide.config(image=divide)
 
-btnresult = tk.Button(root, text="=", command=evaluate_calculation, width=18, font=("Arial",14))
-btnresult.grid(row=7, column=1, columnspan=3)
+btnresult = tk.Button(root, text="=", command=evaluate_calculation,  font=("Consolas",14), bg="white",highlightthickness = 0, bd = 0)
+btnresult.grid(row=7, column=1,columnspan=2,sticky='nesw')
 
-btnclear = tk.Button(root, text="clear", command= clear_field, width=5, font=("Arial",14))
-btnclear.grid(row=7, column=4)
+btndecimal = tk.Button(root, text=".", command=lambda: add_to_calculation("."), font=("Consolas",14), bg="white",highlightthickness = 0, bd = 0)
+btndecimal.grid(row=7, column=3,sticky='nesw')
+
+btnclear = tk.Button(root, text="clear", command= clear_field, font=("Consolas",14), bg="white",highlightthickness = 0, bd = 0)
+btnclear.grid(row=7, column=4,sticky='nesw')
 
 
 
